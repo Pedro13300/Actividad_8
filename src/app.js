3c859c7ce3ae350495e5569a523d2497ec891e84
Pedro José Fernández Merlo <pedrojosefm.91@gmail.com>
2024-05-29 13:35:35 +0200

    creación de carpetas y archivos necesarios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const express = require('express');
 //creacion de la app express
 const app = express();
 
+app.use(express.json());
 
-
+app.use('/api', require('./routes/api'));
 
 module.exports = app;
