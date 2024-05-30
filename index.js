8f5e56ac65e1ab85ee17f88124e008f4e8399503
Pedro José Fernández Merlo <pedrojosefm.91@gmail.com>
2024-05-30 11:20:43 +0200

    peticiones y db.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const http = require('node:http');
 
 const app = require('./src/app');
 
-
 require('dotenv').config();
+
+require ('./src/config/db')
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT);
 
 server.on('listening', () => {
